93ea5c3009585618e0f835faa6edd285f538b05b
echoeyecodes <femiobajuluwa@gmail.com>
2021-01-29 23:14:20 +0100

    added new project - Cashier

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,19 +1,41 @@
-export const projects =[{
-    title:"Sinnerman",
-    image: "https://res.cloudinary.com/dfzhxlgll/image/upload/c_scale,w_1000/v1609179498/one_w3gmrj.webp",
-    description: "An android application for streaming entertaining videos on games and movies I find really exciting from the internet. Features includes likes, comments, and push notifications.",
+export const projects = [
+  {
+    title: "Cashier",
+    image:
+      "https://res.cloudinary.com/dfzhxlgll/image/upload/v1611957964/cashier_dy9ajh.webp",
+    description:
+      "Cashier is a finance app built for companies who are involved with allowing their customers manage their investment with them.  The thought of rebuilding an already existing app using TS seemed like a really nice idea at that moment, so I did!",
+    techs: ["React Native", "NodeJS", "PostgreSQL", "Typescript", "Cloudinary"],
+    link:
+      "https://drive.google.com/file/d/1cjAY2jxkk8wUUF4uiAgy8c33925aLRkS/view?usp=sharing",
+  },
+  {
+    title: "Sinnerman",
+    image:
+      "https://res.cloudinary.com/dfzhxlgll/image/upload/c_scale,w_1000/v1609179498/one_w3gmrj.webp",
+    description:
+      "An android application for streaming entertaining videos on games and movies I find really exciting from the internet. Features includes likes, comments, and push notifications.",
     techs: ["Kotlin", "NodeJS", "PostgreSQL", "Google Cloud Storage", "PubSub"],
-    link: "https://play.google.com/store/apps/details?id=com.echoeyecodes.sinnerman"
-}, {
-    title:"Secure Capital",
-    image: "https://res.cloudinary.com/dfzhxlgll/image/upload/v1609179808/two_d4agf3.webp",
-    description: "Secure Capital is a mobile application for both android and iOS devices built for an investment company (Secure Capital) that allows its clients to easily create, monitor, and manage their investments.",
+    link:
+      "https://play.google.com/store/apps/details?id=com.echoeyecodes.sinnerman",
+  },
+  {
+    title: "Secure Capital",
+    image:
+      "https://res.cloudinary.com/dfzhxlgll/image/upload/v1609179808/two_d4agf3.webp",
+    description:
+      "Secure Capital is a mobile application for both android and iOS devices built for an investment company (Secure Capital) that allows its clients to easily create, monitor, and manage their investments.",
     techs: ["React Native", "NodeJS", "MongoDB", "GraphQL"],
-    link: "https://play.google.com/store/apps/details?id=com.securecapital"
-}, {
-    title:"6TQ Trivia",
-    image: "https://res.cloudinary.com/dfzhxlgll/image/upload/v1609179806/three_ykht5r.webp",
-    description: "6TQ is a mobile quiz application for participating in periodic online quizes. Rewards are based on a cummulative amount (virtual currency) placed before a round starts, which are later equally divided amongst the top 3 players of each round",
+    link: "https://play.google.com/store/apps/details?id=com.securecapital",
+  },
+  {
+    title: "6TQ Trivia",
+    image:
+      "https://res.cloudinary.com/dfzhxlgll/image/upload/v1609179806/three_ykht5r.webp",
+    description:
+      "6TQ is a mobile quiz application for participating in periodic online quizes. Rewards are based on a cummulative amount (virtual currency) placed before a round starts, which are later equally divided amongst the top 3 players of each round",
     techs: ["React Native", "NodeJS", "MongoDB", "GraphQL"],
-    link: "https://drive.google.com/file/d/1t-iUzfOWONnHo4EGGquaF8VC72O8o303/view?usp=sharing"
-}]
+    link:
+      "https://drive.google.com/file/d/1t-iUzfOWONnHo4EGGquaF8VC72O8o303/view?usp=sharing",
+  },
+]
